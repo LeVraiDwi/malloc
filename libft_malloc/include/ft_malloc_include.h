a4fa7b436f0e7b29beca74b1d1f14caa42e439b9
LeVraiDWi <tcosse@student.42.fr>
2023-07-07 17:59:46 +0200

    da

--- libft_malloc/include/ft_malloc_include.h
+++ libft_malloc/include/ft_malloc_include.h
@@ -8,6 +8,7 @@
 # include <unistd.h>
 # include <stdbool.h>
 # include <pthread.h>
+#include <sys/resource.h>
 # include "libft.h"
 
 # define ALIGNEMENT 8
@@ -15,7 +16,7 @@
 # define BLOCK_SIZE(size) (ALIGNE(size + sizeof(t_block)))
 
 # define HEADER_PAGE_SHIFT(page) ((void *)page + sizeof(t_page))
-# define HEADER_BLOCK_DESHIFT(ptr) ((void *)ptr - sizeof(t_page))
+# define HEADER_PAGE_DESHIFT(ptr) ((void *)ptr - sizeof(t_page))
 
 
 # define HEADER_BLOCK_SHIFT(block) ((void *)block + sizeof(t_block))
@@ -26,21 +27,18 @@
 # define MIN_PAGE_BLOCK 128
 
 # define TINY_BLOCK_MAX 256
-# define TINY_BLOCK_SIZE BLOCK_SIZE(TINY_BLOCK_MAX);
-# define TINY_PAGE_SIZE (((TINY_BLOCK_SIZE * MIN_PAGE_BLOCK + sizeof(t_page)) / getpagesize() + 1) * getpagesize())
+# define TINY_BLOCK_SIZE BLOCK_SIZE(TINY_BLOCK_MAX)
+# define TINY_PAGE_SIZE ((((TINY_BLOCK_SIZE * MIN_PAGE_BLOCK) + sizeof(t_page)) / getpagesize() + 1) * getpagesize())
 
 # define SMALL_BLOCK_MAX 2048
-# define SMALL_BLOCK_SIZE BLOCK_SIZE(SMALL_BLOCK_MAX);
-# define SMALL_PAGE_SIZE (((SMALL_BLOCK_SIZE * MIN_PAGE_BLOCK + sizeof(t_page)) / getpagesize() + 1) * getpagesize())
+# define SMALL_BLOCK_SIZE BLOCK_SIZE(SMALL_BLOCK_MAX)
+# define SMALL_PAGE_SIZE ((((SMALL_BLOCK_SIZE * MIN_PAGE_BLOCK) + sizeof(t_page)) / getpagesize() + 1) * getpagesize())
 
-# define LARGE_PAGE_SIZE(size) (((BLOCK_SIZE(size) + sizeof(t_page)) / getpagesize() + 1) * getpagesize())
-
-extern t_heap g_heap;
-extern pthread_mutex_t g_heap_mutex;
+# define LARGE_PAGE_SIZE(size) (((size + sizeof(t_page)) / getpagesize() + 1) * getpagesize())
 
 typedef struct s_page t_page;
 
-typedef s_block {
+typedef struct s_block {
     size_t          size;
     bool            allocated;
     t_page          *parent;
@@ -49,24 +47,27 @@ typedef s_block {
     struct s_block  *prev;
 }       t_block;
 
-typedef s_page {
+typedef struct s_page {
     size_t          size;
     size_t          used_size;
     unsigned int    nb_block;
     unsigned int    nb_freed;
     t_block         *block;
-    struct s_page   next;
-    struct s_page   prev;
+    struct s_page   *next;
+    struct s_page   *prev;
 }       t_page;
 
-typedef s_zone {
+typedef struct s_zone {
     t_page          *page;
     unsigned int    nb_page;
 }       t_zone;
 
-typedef s_heap {
-    t_zone  *tiny;
-    t_zone  *small;
-    t_zone  *large;
+typedef struct s_heap {
+    t_zone  tiny;
+    t_zone  small;
+    t_zone  large;
 }       t_heap;
+
+extern t_heap g_heap;
+extern pthread_mutex_t g_heap_mutex;
 #endif
--- src/main.c
+++ src/main.c
@@ -3,8 +3,21 @@
 
 int main() {
     int *i = (int *)ft_malloc(sizeof(int));
+    char *c = (char *)ft_malloc(sizeof(char) * 8);
+    char *c_ = (char *)malloc(sizeof(char) * 8);
 
+    int *tabInt = (int *)ft_malloc(sizeof(int) * 1024);
+
+    c[0] = 's';
+    c[1] = 'a';
+    c[2] = 'l';
+    c[3] = '\n';
+    c[4] = '\0';
+    c[234] = 'q';
+    c_[234] = 'q';
     *i = 2;
-    printf("%d\n", *i);
+    tabInt[1024] = 11;
+    tabInt[1000] = 2312;
+    printf("%d\n%s%d\n%c\n%c\n%d\n", *i, c, tabInt[1000], c[234], c_[234], tabInt[1024]);
     return 1;
 }
